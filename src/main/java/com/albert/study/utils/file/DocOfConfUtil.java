3d6a38f9ec3b7c3102eaa62cdb6650de850ab6fd
zztiyjw <18438049166@163.com>
2020-08-03 22:36:01 +0800

    # 提交apache读取文件工具类

--- src/main/java/com/albert/study/utils/file/DocOfConfUtil.java
+++ src/main/java/com/albert/study/utils/file/DocOfConfUtil.java
@@ -1,11 +1,28 @@
 package com.albert.study.utils.file;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 /**
  * @author yjw
  * @date 2020/8/3 21:44
  */
+@Slf4j
 public class DocOfConfUtil {
 
+    public static String getContentInPath(String path) {
+        File file = new File(path);
+        try {
+            return FileUtils.readFileToString(file, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            log.error("read user message from file error", e);
+        }
+        return null;
+    }
 
 
 }
