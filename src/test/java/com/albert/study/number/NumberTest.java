d6088c6a839d4172f13948b69428cf0c0f65082f
albert.yang <Yang161455>
2020-08-15 18:30:27 +0800

    # 提交数字操作的练习类

--- src/test/java/com/albert/study/number/NumberTest.java
+++ src/test/java/com/albert/study/number/NumberTest.java
@@ -1,11 +1,20 @@
 package com.albert.study.number;
 
 import com.albert.study.TestApplication;
+import com.albert.study.utils.jackson.utils.JsonUtil;
+import com.albert.study.utils.localdatetime.utils.LocalDateTimeUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Albert
@@ -18,18 +27,89 @@ public class NumberTest {
 
     /**
      * 测试Double类型转Int类型
-     *
      */
     @Test
-    public void testDoubleToInt(){
+    public void testDoubleToInt() {
         Double num = 2.99;
         //强转，只取整数位
         int i = num.intValue();
-        log.info("double to int : {}",i );
+        log.info("double to int : {}", i);
+    }
+
+    @Test
+    public void test1() {
+        Double a = 0.00;
+        System.out.println(a.intValue());
+
+        Double b = 0.01;
+        System.out.println(b.intValue());
+
+
+        System.out.println(a == 0.00D);
+        System.out.println(b == 0.00D);
 
 
     }
 
+    @Test
+    public void test2() {
+        System.out.println(getLossListByType("day"));
+
+    }
+
+    private List<Object> getLossListByType(String type) {
+        switch (type) {
+            case "day":
+                return getLossListByHour();
+            case "week":
+                return getLossListByWeek();
+            case "month":
+                return getLossListByMonth();
+        }
+        return Lists.newArrayList();
+    }
+
+    /**
+     * 查询month的数据
+     * @return
+     */
+    private List<Object> getLossListByMonth(){
+        ArrayList<Object> objects = Lists.newArrayList();
+        objects.add("1");
+        return objects;
+
+    }
+
+    /**
+     * 查询week或month的数据
+     * @return
+     */
+    private List<Object> getLossListByWeek(){
+        ArrayList<Object> objects = Lists.newArrayList();
+        objects.add("2");
+        return objects;
+    }
+
+    /**
+     * 查询hour的数据
+     * @return
+     */
+    private List<Object> getLossListByHour(){
+        ArrayList<Object> objects = Lists.newArrayList();
+        objects.add("3");
+        return objects;
+    }
+
+    @Test
+    public void testTime(){
+
+        String a = "abc";
+
+        JsonNode activeTraceBos = JsonUtil.getJsonNode(a).get("data").get("activeTraceBos");
+
+
+
+    }
 
 
 }
