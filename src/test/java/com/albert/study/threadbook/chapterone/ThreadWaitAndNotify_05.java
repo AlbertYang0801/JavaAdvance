e234c11177539e522e16d68fd610c8a8c75cff4c
albert.yang <Yang161455>
2020-08-26 19:50:25 +0800

    # 提交并发编程例子练习

--- src/test/java/com/albert/study/threadbook/chapterone/ThreadWaitAndNotify_05.java
+++ src/test/java/com/albert/study/threadbook/chapterone/ThreadWaitAndNotify_05.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.sql.Time;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
