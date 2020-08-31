50f5d8751a54708180335d9d2849a1bbebfa8ba8
albert.yang <Yang161455>
2020-08-31 16:54:51 +0800

    # 练习线程并发

---
+++ src/test/java/com/albert/study/threadbook/chapterone/ThreadPriority_10.java
@@ -0,0 +1,63 @@
+package com.albert.study.threadbook.chapterone;
+
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+/**
+ * 测试线程优先级
+ * 例10_对应59页的例子
+ *
+ * @author Albert
+ * @date 2020/8/31 14:41
+ */
+public class ThreadPriority_10 {
+
+    public static class HignPriority implements Runnable {
+        private static int count = 0;
+
+        @Override
+        public void run() {
+            while (true) {
+                count++;
+                if (count > 10000000) {
+                    System.out.println("HignPriority 抢到了锁");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static class LowPriority extends Thread {
+        private static int count = 0;
+
+        @Override
+        public void run() {
+            while (true) {
+                count++;
+                if (count > 10000000) {
+                    System.out.println("LowPriority 抢到了锁");
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 测试设置线程优先级
+     * 线程间抢夺资源，优先级越高，越容易抢到。
+     */
+    @SneakyThrows
+    @Test
+    public void testPriority() {
+        Thread highThread = new Thread(new HignPriority());
+        Thread lowThread = new Thread(new LowPriority());
+        //设置优先级
+        highThread.setPriority(Thread.MAX_PRIORITY);
+        lowThread.setPriority(Thread.MIN_PRIORITY);
+        lowThread.start();
+        highThread.start();
+        Thread.sleep(1000);
+    }
+
+
+}
