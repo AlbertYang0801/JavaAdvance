af62915bee4a49b6c66f93606cb61e251db0f3f7
albert.yang <18438049166@163.com>
2020-11-03 12:13:15 +0800

    # 添加邮箱功能测试方法

--- mail-demo/src/test/java/com/albert/mail/send/TestSendMaill.java
+++ mail-demo/src/test/java/com/albert/mail/send/TestSendMaill.java
@@ -4,15 +4,12 @@ import com.albert.mail.TestApplication;
 import com.albert.mail.common.MailUtilHandler;
 import com.albert.mail.job.SendMailTask;
 import com.albert.mail.utils.MailUtil;
-import com.albert.utils.localdatetime.LocalDateTimeUtils;
 import com.google.common.collect.Lists;
 import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
