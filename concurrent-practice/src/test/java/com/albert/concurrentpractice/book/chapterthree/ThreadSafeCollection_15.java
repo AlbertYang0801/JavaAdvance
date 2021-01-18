8344285338d0a0ec3f530cef011eb80c5b017cf5
albert.yang <18438049166@163.com>
2021-01-18 18:30:25 +0800

    # 提交Collections封装线程安全集合的例子

--- concurrent-practice/src/test/java/com/albert/concurrentpractice/book/chapterthree/ThreadSafeCollection_15.java
+++ concurrent-practice/src/test/java/com/albert/concurrentpractice/book/chapterthree/ThreadSafeCollection_15.java
@@ -1,17 +1,20 @@
 package com.albert.concurrentpractice.book.chapterthree;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * 线程安全的Map练习
+ * 线程安全的集合练习
  * @author Albert
  * @date 2021/1/18 下午6:22
  */
-public class ThreadSafeMap_15 {
+public class ThreadSafeCollection_15 {
 
     public static void main(String[] args) {
         Map<String,String> oldMap = Maps.newHashMap();
@@ -20,7 +23,18 @@ public class ThreadSafeMap_15 {
 
         //线程安全的Map
         ConcurrentMap<Object, Object> map = Maps.newConcurrentMap();
+
+
+        ArrayList<Object> oldList = Lists.newArrayList();
+        //使用Collections对list进行线程同步封装
+        List<Object> safeList = Collections.synchronizedList(oldList);
     }
 
 
 }
+
+
+/**
+ * Collections封装的线程安全的集合，是因为其内部有个object对象，以该对象作为锁加在集合的相关方法上。
+ * 在每个方法执行之前都要获得对象锁，效率较低。
+ */
