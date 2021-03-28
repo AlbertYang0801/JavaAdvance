2c680c044586d09194fc79e4442d84a4f1daa545
albert.yang <18438049166@163.com>
2021-03-29 01:05:38 +0800

    # 提交集合练习代码

--- spring-practice/src/main/java/com/albert/spring/collection/ListDemo.java
+++ spring-practice/src/main/java/com/albert/spring/collection/ListDemo.java
@@ -1,8 +1,11 @@
 package com.albert.spring.collection;
 
 import com.albert.utils.jackson.JsonUtil;
+import com.albert.utils.localdatetime.LocalDateTimeUtils;
+import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.*;
 
 /**
  * @author Albert
@@ -10,13 +13,68 @@ import java.util.ArrayList;
  */
 public class ListDemo {
 
-    public static void main(String[] args) {
+    @Test
+    public void arrayListDemo() {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         System.out.println(JsonUtil.toString(list));
-        //只能添加已有元素的位置，和末尾+1
-        list.add(1,2);
+        //只能在已有元素的位置，和末尾+1的位置添加元素。
+        list.add(1, 2);
         System.out.println(JsonUtil.toString(list));
     }
+
+    @Test
+    public void testListIteator(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println(next);
+            list.add("d");
+        }
+        System.out.println(JsonUtil.toString(list));
+    }
+
+    @Test
+    public void linkedListDemo() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(2);
+        linkedList.remove(2);
+        System.out.println(JsonUtil.toString(linkedList));
+        Integer integer = linkedList.get(2);
+        System.out.println(integer);
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext()){
+            linkedList.remove(2);
+        }
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void vectorDemo() {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        List<Integer> list = Collections.synchronizedList(vector);
+        list.get(0);
+        //向右位运算
+        int b = 8 >> 1;
+        System.out.println(b);
+        int a = 1;
+        list.add(a);
+    }
+
+    @Test
+    public void testYYYY(){
+        String s = LocalDateTimeUtils.formatTime(LocalDateTime.now(), "yyyy-MM-dd HH:mm:ss:SSS");
+        System.out.println(s);
+    }
+
+
 
 }
