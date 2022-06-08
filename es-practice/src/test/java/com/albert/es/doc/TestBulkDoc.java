3b90faa38e09b0207ed48f77a98f016201462633
yangjunwei <18438049166@163.com>
2022-06-08 17:42:52 +0800

    es的bulk操作

--- es-practice/src/test/java/com/albert/es/doc/TestBulkDoc.java
+++ es-practice/src/test/java/com/albert/es/doc/TestBulkDoc.java
@@ -1,24 +1,17 @@
 package com.albert.es.doc;
 
+import cn.hutool.core.date.DateUtil;
 import com.albert.es.TestApplication;
 import com.albert.es.config.EsBulkProcessor;
-import java.io.IOException;
+import com.alibaba.fastjson.JSON;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Resource;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.bulk.BulkProcessor;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
+import lombok.SneakyThrows;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.rest.RestStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,11 +29,69 @@ public class TestBulkDoc {
     @Autowired
     EsBulkProcessor esBulkProcessor;
 
-    /**
-     * 添加文档的测试
-     */
+    @Autowired
+    RestHighLevelClient restHighLevelClient;
+
+    @SneakyThrows
     @Test
     public void testBulk() throws InterruptedException {
+        System.out.println("start!!" + DateUtil.now());
+        BulkRequest bulkRequest = new BulkRequest();
+        for (int i = 0; i < 1000; i++) {
+            //创建文档数据
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "基础");
+            map.put("time", "2022-03-15 16:04:24");
+            map.put("price", i);
+            //索引请求对象
+            IndexRequest indexRequest = new IndexRequest("test-0602", "_doc");
+            //指定索引文档内容
+            indexRequest.source(map);
+            bulkRequest.add(indexRequest);
+        }
+        restHighLevelClient.bulk(bulkRequest);
+        System.out.println("end!!" + DateUtil.now());
+        Thread.sleep(50000);
+    }
+
+    @SneakyThrows
+    @Test
+    public void testBulkAsync() {
+        System.out.println("start!!" + DateUtil.now());
+        BulkRequest bulkRequest = new BulkRequest();
+        for (int i = 0; i < 1000; i++) {
+            //创建文档数据
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "基础");
+            map.put("time", "2022-03-15 16:04:24");
+            map.put("price", i);
+            //索引请求对象
+            IndexRequest indexRequest = new IndexRequest("test-0602", "_doc");
+            //指定索引文档内容
+            indexRequest.source(map);
+            bulkRequest.add(indexRequest);
+        }
+        restHighLevelClient.bulkAsync(bulkRequest, new ActionListener<BulkResponse>() {
+            @Override
+            public void onResponse(BulkResponse bulkItemResponses) {
+                System.out.println(JSON.toJSONString(bulkItemResponses));
+                System.out.println("响应时间" + bulkItemResponses.getTook().getSeconds());
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                System.out.println("执行失败");
+            }
+        });
+        System.out.println("end!!" + DateUtil.now());
+        Thread.sleep(50000);
+    }
+
+    @SneakyThrows
+    @Test
+    public void testBulkProcessor() {
+        System.out.println("start!!" + DateUtil.now());
+        BulkRequest bulkRequest = new BulkRequest();
         for (int i = 0; i < 1000; i++) {
             //创建文档数据
             Map<String, Object> map = new HashMap<>();
@@ -53,6 +104,7 @@ public class TestBulkDoc {
             indexRequest.source(map);
             esBulkProcessor.getBulkProcessor().add(indexRequest);
         }
+        System.out.println("end!!" + DateUtil.now());
         Thread.sleep(50000);
     }
 
