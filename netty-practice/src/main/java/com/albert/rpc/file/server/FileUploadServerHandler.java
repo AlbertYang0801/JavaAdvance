b8be3a175801623d3bf5a2bffb290e8d38e967cb
yangjunwei <yangjunwei@harmonycloud.cn>
2024-06-12 17:22:35 +0800

    netty fileupload

---
+++ netty-practice/src/main/java/com/albert/rpc/file/server/FileUploadServerHandler.java
@@ -0,0 +1,11 @@
+package com.albert.rpc.file.server;
+
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author yangjunwei
+ * @date 2024-06-12
+ */
+public class FileUploadServerHandler extends ChannelInboundHandlerAdapter {
+}
